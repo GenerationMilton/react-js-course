99dfd29d1376071524b49db8d490fd6ba7760b81
Milton Muñoz <livemilton@gmail.com>
2024-09-16 21:21:34 -0500

    import and export in js

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+//import { apiKey} from "./util.js";
+
+//import apiKey from './util.js';
+//import{apiKey,abc} from "./util.js";
+//import * as util from "./util.js";
+import {apiKey, abc as content} from "./util.js";
+
+//console.log(util.apiKey);
+//console.log(util.abc);
+
+console.log(content);
---
+++ assets/scripts/util.js
@@ -0,0 +1,5 @@
+//export let apiKey="adkgrflgrpqwertopy";
+
+export default "adkgrflgrpqwertopy";
+export let apiKey= "klotiyutjgfa";
+export let abc="abc";
