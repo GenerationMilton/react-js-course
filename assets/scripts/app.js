d4bec47c59100f0a8cac510bdd06e8b6e8c6ea0d
Milton Muñoz <livemilton@gmail.com>
2024-09-16 21:44:08 -0500

    variables and constants in JS

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,18 @@
 //import apiKey from './util.js';
 //import{apiKey,abc} from "./util.js";
 //import * as util from "./util.js";
-import {apiKey, abc as content} from "./util.js";
+//import {apiKey, abc as content} from "./util.js";
 
 //console.log(util.apiKey);
 //console.log(util.abc);
 
-console.log(content);
+//console.log(content);
+
+//variables-uses
+let userMessage = "Hello World!";
+console.log(userMessage);
+console.log(userMessage);
+
+//constants
+const userMessage2="Hello World again";
+console.log(userMessage2);
