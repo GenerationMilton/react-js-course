54ce215c8a2cebe82066073e8ee50c3c1aff3856
Milton Muñoz <livemilton@gmail.com>
2024-09-16 21:49:27 -0500

    values and operators in js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,3 +18,15 @@ console.log(userMessage);
 //constants
 const userMessage2="Hello World again";
 console.log(userMessage2);
+
+
+//Values and operators
+console.log("Hello"+"Maria");
+console.log(10===5);
+console.log(10===10);
+console.log(10<=10);
+console.log(10>=10);
+
+if(10===10){
+    console.log("works");
+}
